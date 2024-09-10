afd8f367f129ef170c23a750fb64e8b3d90f7d96
web-barina <why37mr70@gmail.com>
2024-09-10 22:56:18 +0900

    09/10

--- WordPressTheme/assets/js/gsap.js
+++ WordPressTheme/assets/js/gsap.js
@@ -12,119 +12,154 @@ textWrapBlog.forEach(function (t) {
 
 //ローディング画面
 // カーテンを左右に開くアニメーション
-gsap.timeline().to(".js-loading-left", {
-  x: "-100%",
-  duration: 3,
-  ease: "power2.out"
-}).to(".js-loading-right", {
-  x: "100%",
-  duration: 3,
-  ease: "power2.out",
-  onComplete: function onComplete() {
-    gsap.set(".js-loading", {
-      display: "none"
-    });
-  }
-}, "-=3.0");
+gsap
+  .timeline()
+  .to(".js-loading-left", {
+    x: "-100%",
+    duration: 3,
+    ease: "power2.out",
+  })
+  .to(
+    ".js-loading-right",
+    {
+      x: "100%",
+      duration: 3,
+      ease: "power2.out",
+      onComplete: function onComplete() {
+        gsap.set(".js-loading", {
+          display: "none",
+        });
+      },
+    },
+    "-=3.0"
+  );
 
 // TOP アニメーション
-gsap.fromTo(".topFVswiper__texts", {
-  y: "20%",
-  opacity: 0
-}, {
-  y: 0,
-  duration: 2,
-  delay: 1.6,
-  opacity: 1
-});
+gsap.fromTo(
+  ".topFVswiper__texts",
+  {
+    y: "20%",
+    opacity: 0,
+  },
+  {
+    y: 0,
+    duration: 2,
+    delay: 1.6,
+    opacity: 1,
+  }
+);
 
 // タイムラインに scrollTrigger を設定
 var tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".top-biography__img",
-    start: "top 80%"
+    start: "top 80%",
+  },
+});
+tl.fromTo(
+  ".top-biography__img",
+  {
+    x: "-20%",
+    opacity: 0,
+  },
+  {
+    x: 0,
+    duration: 1.5,
+    opacity: 1,
   }
-});
-tl.fromTo(".top-biography__img", {
-  x: "-20%",
-  opacity: 0
-}, {
-  x: 0,
-  duration: 1.5,
-  opacity: 1
-}).fromTo(".top-biography__content", {
-  y: "20%",
-  opacity: 0
-}, {
-  y: 0,
-  duration: 1.5,
-  opacity: 1
-});
+).fromTo(
+  ".top-biography__content",
+  {
+    y: "20%",
+    opacity: 0,
+  },
+  {
+    y: 0,
+    duration: 1.5,
+    opacity: 1,
+  }
+);
 
 // .top-performance__title の文字をフェードインさせるアニメーション
-gsap.fromTo(".top-performance-span", {
-  opacity: 0,
-  y: 20
-}, {
-  opacity: 1,
-  y: 0,
-  duration: 0.5,
-  stagger: 0.1,
-  scrollTrigger: {
-    trigger: ".top-performance__title",
-    start: "top 80%"
+gsap.fromTo(
+  ".top-performance-span",
+  {
+    opacity: 0,
+    y: 20,
+  },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 0.5,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: ".top-performance__title",
+      start: "top 80%",
+    },
   }
-});
-gsap.fromTo(".top-performance__content", {
-  opacity: 0,
-  y: "20%"
-}, {
-  opacity: 1,
-  y: 0,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".top-performance__content",
-    start: "top 80%"
+);
+gsap.fromTo(
+  ".top-performance__content",
+  {
+    opacity: 0,
+    y: "20%",
+  },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".top-performance__content",
+      start: "top 80%",
+    },
   }
-});
+);
 gsap.from(".blog-cards__item", {
   scrollTrigger: {
     trigger: ".blog__cards",
-    start: "top 80%"
+    start: "top 80%",
   },
   y: 50,
   opacity: 0,
   duration: 1,
   ease: "power2.out",
-  stagger: 0.5
+  stagger: 0.5,
 });
 
 // .top-blog__title の文字をフェードインさせるアニメーション
-gsap.fromTo(".top-blog-span", {
-  opacity: 0,
-  y: 20
-}, {
-  opacity: 1,
-  y: 0,
-  duration: 0.5,
-  stagger: 0.1,
-  scrollTrigger: {
-    trigger: ".top-blog__title",
-    start: "top 80%"
+gsap.fromTo(
+  ".top-blog-span",
+  {
+    opacity: 0,
+    y: 20,
+  },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 0.5,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: ".top-blog__title",
+      start: "top 80%",
+    },
   }
-});
-gsap.fromTo(".contact__inner", {
-  opacity: 0,
-  y: "20%"
-}, {
-  opacity: 1,
-  y: 0,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".contact__inner",
-    start: "top 80%"
+);
+gsap.fromTo(
+  ".contact__inner",
+  {
+    opacity: 0,
+    y: "20%",
+  },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".contact__inner",
+      start: "top 80%",
+    },
   }
-});
+);
 jQuery(function ($) {
   // この中であればWordpressでも「$」が使用可能になる
 
@@ -167,8 +202,8 @@ jQuery(function ($) {
     speed: 1500,
     autoplay: {
       delay: 3000,
-      disableOnInteraction: false
-    }
+      disableOnInteraction: false,
+    },
   });
 
   /**************
@@ -185,18 +220,27 @@ jQuery(function ($) {
     color.css("width", "0%");
     color.on("inview", function () {
       if (counter == 0) {
-        $(this).delay(200).animate({
-          width: "100%"
-        }, speed, function () {
-          image.css("opacity", "1");
-          $(this).css({
-            left: "0",
-            right: "auto"
-          });
-          $(this).animate({
-            width: "0%"
-          }, speed);
-        });
+        $(this)
+          .delay(200)
+          .animate(
+            {
+              width: "100%",
+            },
+            speed,
+            function () {
+              image.css("opacity", "1");
+              $(this).css({
+                left: "0",
+                right: "auto",
+              });
+              $(this).animate(
+                {
+                  width: "0%",
+                },
+                speed
+              );
+            }
+          );
         counter = 1;
       }
     });
@@ -239,29 +283,34 @@ jQuery(function ($) {
   });
   // #scroll-topをクリックした際の設定
   $("#js-scroll-top").click(function () {
-    $("body,html").animate({
-      scrollTop: 0 //ページトップまでスクロール
-    }, 500); //ページトップスクロールの速さ。
+    $("body,html").animate(
+      {
+        scrollTop: 0, //ページトップまでスクロール
+      },
+      500
+    ); //ページトップスクロールの速さ。
     return false; //リンク自体の無効化
   });
 
   /******************
    *AboutUs モーダル*
    *****************/
-  $(document).ready($(function () {
-    $(".photo").on("click", function () {
-      var modal_id = $(this).attr("id");
-      $(".modal#cont-" + modal_id).fadeIn(200);
-      $(".modal#cont-" + modal_id).addClass("active");
-      $("html, body").css("overflow-y", "hidden");
-    });
-    $(".modal").on("click", function () {
-      if ($(this).hasClass("active")) {
-        $(this).fadeOut();
-        $("html, body").css("overflow-y", "auto");
-      }
-    });
-  }));
+  $(document).ready(
+    $(function () {
+      $(".photo").on("click", function () {
+        var modal_id = $(this).attr("id");
+        $(".modal#cont-" + modal_id).fadeIn(200);
+        $(".modal#cont-" + modal_id).addClass("active");
+        $("html, body").css("overflow-y", "hidden");
+      });
+      $(".modal").on("click", function () {
+        if ($(this).hasClass("active")) {
+          $(this).fadeOut();
+          $("html, body").css("overflow-y", "auto");
+        }
+      });
+    })
+  );
 
   /********************
    *サイドバーアーカイブ*
